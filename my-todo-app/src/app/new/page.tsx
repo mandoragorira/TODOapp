fcbd39b894c32e2d13b602ceb672840629f8a8a8
Miyu Akiyama <mi-akiyama@clinks.jp>
2025-06-19 10:43:22 +0900

    new file

---
+++ my-todo-app/src/app/new/page.tsx
@@ -0,0 +1,9 @@
+// app/new/page.tsx
+export default function NewTaskPage() {
+  return (
+    <main style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>新しいタスクを作成するページだよ！</h1>
+      <p>ここからタスク作成のフォームを作るんだ。</p>
+    </main>
+  );
+}
